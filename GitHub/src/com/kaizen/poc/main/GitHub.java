fceb3afa00ebc60e9212deb16e903fae9dc636ac
jfajardo <jfajardo@aeon.credit.com.ph>
2016-08-08 08:29:21 +0800

    Thi is my first commit in GITHUB!

---
+++ GitHub/src/com/kaizen/poc/main/GitHub.java
@@ -0,0 +1,15 @@
+package com.kaizen.poc.main;
+
+/*
+ * @author jfajardo
+ * version 1.0
+ * 2016-08-07
+ */
+public class GitHub {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello GitHub this is my first commit!");
+		
+	}
+}
